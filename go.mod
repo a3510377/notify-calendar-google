b446e29917287a4b9f876daf9c07753058f31ddf
猴貓 <a102009102009@gmail.com>
2023-02-14 22:54:38 +0800

    feat cron call

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/joho/godotenv v1.5.1
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 )
 
 // c_nbtiskrng1pkrcj168db62l4hg@group.calendar.google.com
@@ -27,42 +28,42 @@ func main() {
 		panic("CALENDAR_ID is empty")
 	}
 
-	baseLoop := func() {
-		nowTime := time.Now().AddDate(0, 0, 2)
-		resp, err := http.Get(NewCalendarV3ApiRequest(nowTime, CALENDAR_ID).BaseURL().String())
-		if err != nil {
-			log.Println("Error getting calendar data: ", err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 200 {
-			log.Println("Error getting calendar data: ", resp.Status)
-			return
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Println("Error reading calendar data: ", err)
-			return
-		}
-		data := CalenderV3ApiResponse{}
-		json.Unmarshal(body, &data)
-
-		log.Println("Calendar data: ", data)
-		for _, item := range data.Items {
-			// check if the item start time is before the current time and the status is confirmed
-			if nowTime.Format("2006-01-02") == item.Start.Date && item.Status == "confirmed" {
-				notification(item)
-			}
-		}
-	}
-
-	baseLoop()
-	for range time.Tick(time.Hour * 24) { // 24 hour clock
-		baseLoop()
-	}
+	c := cron.New()
+	// TODO add config cron rule
+	c.AddFunc("0 0 12 * * ?", func() { checkAndNotification(CALENDAR_ID) })
+	c.Start() // loop start
 }
 
 func notification(data CalenderV3ApiEventData) {
+}
+
+func checkAndNotification(CALENDAR_ID string) {
+	nowTime := time.Now().AddDate(0, 0, 2)
+	resp, err := http.Get(NewCalendarV3ApiRequest(nowTime, CALENDAR_ID).BaseURL().String())
+	if err != nil {
+		log.Println("Error getting calendar data: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Println("Error getting calendar data: ", resp.Status)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error reading calendar data: ", err)
+		return
+	}
+	data := CalenderV3ApiResponse{}
+	json.Unmarshal(body, &data)
+
+	log.Println("Calendar data: ", data)
+	for _, item := range data.Items {
+		// check if the item start time is before the current time and the status is confirmed
+		if nowTime.Format("2006-01-02") == item.Start.Date && item.Status == "confirmed" {
+			notification(item)
+		}
+	}
 }
